89207183bae9b5cbf6edf66280cf71d5c397a20d
kridsadacpe <kridsadacpe@gmail.com>
2017-08-09 01:15:55 +0700

    Add ChainOfResp

---
+++ Behavioral Design Pattern/Chain of responsibility/ChainOfRespCore/ChainOfRespCore/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfRespCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Handle handle1 = new ConcreateHandle1();
+            Handle handle2 = new ConcreateHandle2();
+            Handle handle3 = new ConcreateHandle3();
+
+            handle1.SetSuccessor(handle2);
+            handle2.SetSuccessor(handle3);
+
+            // Generate and process request
+            int[] requests = { 2, 5, 14, 22, 18, 3, 27, 20 };
+
+            foreach (int request in requests)
+            {
+                handle1.HandleRequest(request);
+            }
+
+            //Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Handle
+    {
+        protected Handle successor;
+
+        public void SetSuccessor(Handle successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void HandleRequest(int request);
+    }
+
+    class ConcreateHandle1 : Handle
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request > 0 && request < 10)
+            {
+                Console.WriteLine("{0} handled request {1}",
+                    this.GetType().Name, request);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+    class ConcreateHandle2 : Handle
+    {
+        public override void HandleRequest(int request)
+        {
+            if(request >= 10 && request > 20)
+            {
+                Console.WriteLine("{0} handled request {1}",
+                    this.GetType().Name, request);
+            } else if(successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+    class ConcreateHandle3 : Handle
+    {
+        public override void HandleRequest(int request)
+        {
+            if(request >= 20 && request < 30 )
+            {
+                Console.WriteLine("{0} handled request {1}",
+                    this.GetType().Name, request);
+            } else
+            {
+                if(successor != null)
+                {
+                    successor.HandleRequest(request);
+                }
+            }
+        }
+    }
+}
---
+++ Behavioral Design Pattern/Chain of responsibility/ChainOfRespRealWorld/ChainOfRespRealWorld/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfRespRealWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Set up chain of responsibility
+            Approver larry = new Director();
+            Approver sam = new VicePrisident();
+            Approver tammy = new Prisedent();
+
+            larry.SetSuccessor(sam);
+            sam.SetSuccessor(tammy);
+
+
+            // Generate and process purchase requests
+            Purchase p = new Purchase(2034, 350, "Assets");
+            larry.ProcessRequest(p);
+
+            p = new Purchase(2035, 32590.10, "Project X");
+            larry.ProcessRequest(p);
+
+            p = new Purchase(2036, 122100.00, "Project Y");
+            larry.ProcessRequest(p);
+
+            //Wait for user key
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Approver
+    {
+        protected Approver successor;
+
+        public void SetSuccessor(Approver successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void ProcessRequest(Purchase purchase);
+    }
+
+    class Director : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if(purchase.Amount < 10000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}",
+                    this.GetType().Name, purchase.Number);
+            } else if(successor != null)
+            {
+                successor.ProcessRequest(purchase);
+            }
+        }
+    }
+
+    class VicePrisident : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 25000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}",
+                    this.GetType().Name, purchase.Number);
+            }
+            else if (successor != null)
+            {
+                successor.ProcessRequest(purchase);
+            }
+        }
+    }
+
+    class Prisedent : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 100000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}",
+                    this.GetType().Name, purchase.Number);
+            }
+            else
+            {
+                Console.WriteLine(
+                  "Request# {0} requires an executive meeting!",
+                  purchase.Number);
+            }
+        }
+    }
+
+
+
+    public class Purchase
+    {
+        private int _number;
+        private double _amount;
+        private string _purpose;
+
+        public Purchase(int number, double amount, string purpose)
+        {
+            _number = number;
+            _amount = amount;
+            _purpose = purpose;
+        }
+
+        public int Number
+        {
+            get { return this._number; }
+            set { this._number = value; }
+        }
+
+        public double Amount
+        {
+            get { return this._amount; }
+            set { this._amount = value ; }
+        }
+
+        public string Purpose
+        {
+            get { return this._purpose; }
+            set { this._purpose = value; }
+        }
+    }
+}
