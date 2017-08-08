fc802f6f09c58d29cd3f5f473a5866d49d2a4318
kridsadacpe <kridsadacpe@gmail.com>
2017-08-08 14:48:36 +0700

    Add facade class

---
+++ Structerral Design Pattern/Facade/FacadeCore/FacadeCore/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+            facade.MethodA();
+            facade.MethodB();
+
+            Console.ReadKey();
+        }
+    }
+
+    class SubSysytemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine(" SubSystemOne Method");
+        }
+    }
+
+    class SubSystemTwo
+    {
+        public void MethodTwo()
+        {
+            Console.WriteLine(" SubSystemTwo Method");
+        }
+    }
+
+    class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine(" SubSystemThree Method");
+        }
+    }
+
+    class SubSystemFour
+    {
+        public void MethodFour()
+        {
+            Console.WriteLine(" SubSystemFour Method");
+        }
+    }
+
+    class Facade
+    {
+        private SubSysytemOne _one;
+        private SubSystemTwo _two;
+        private SubSystemThree _three;
+        private SubSystemFour _four;
+        public Facade()
+        {
+            _one = new SubSysytemOne();
+            _two = new SubSystemTwo();
+            _three = new SubSystemThree();
+            _four = new SubSystemFour();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("\nMethodA() ---- ");
+            _one.MethodOne();
+            _two.MethodTwo();
+            _four.MethodFour();
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("\nMethodB() ---- ");
+            _two.MethodTwo();
+            _three.MethodThree();
+        }
+
+    }
+}
---
+++ Structerral Design Pattern/Facade/FacadeRealWorld/FacadeRealWorld/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeRealWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MortGage mortGage = new MortGage();
+
+            // Evaluate mortgage eligibility for customer
+            Customer customer = new Customer("Ann Mckinsey");
+            bool eligible = mortGage.IsEligible(customer, 125000);
+            Console.WriteLine("\n" + customer.Name + 
+                " Has been " + (eligible ? "Approved" : "Rejected"));
+
+            Console.ReadKey();
+        }
+    }
+
+    class Bank
+    {
+        public bool HasSufficientSavings(Customer c, int amount)
+        {
+            Console.WriteLine("Check bank for " + c.Name);
+            return true;
+        }
+    }
+
+    class Credit
+    {
+        public bool HaveGoodCredit(Customer c)
+        {
+            Console.WriteLine("Check credit for " + c.Name);
+            return true;
+        }
+    }
+
+    class Loan
+    {
+        public bool HasNoBadLoans(Customer c)
+        {
+            Console.WriteLine("Check loans for " + c.Name);
+            return true;
+        }
+    }
+
+    public class Customer
+    {
+        private string _name;
+
+        public Customer(string name)
+        {
+            _name = name;
+        }
+
+        public string Name {
+            get { return this._name;  }
+            set { this.Name = value; }
+        }
+    }
+
+    /// <summary>
+    /// The 'Facade' class
+    /// </summary>
+    class MortGage
+    {
+        private Bank _bank = new Bank();
+        private Loan _loan = new Loan();
+        private Credit _credit = new Credit();
+
+        public bool IsEligible(Customer c, int amount)
+        {
+            Console.WriteLine("{0} applies for {1:C} loan\n", c.Name, amount);
+
+            bool eligible = true;
+
+            // Check creditworthyness of applicant
+            if (!_bank.HasSufficientSavings(c, amount))
+            {
+                eligible = false;
+            }
+            else if (!_loan.HasNoBadLoans(c))
+            {
+                eligible = false;
+            }
+            else if (!_credit.HaveGoodCredit(c))
+            {
+                eligible = false;
+            }
+
+            return eligible;
+        }
+    }
+}
