cad1334b576c446afa0cea72d31d49161b47b317
kridsadacpe <kridsadacpe@gmail.com>
2017-08-10 22:27:41 +0700

    Add Template method pattern

---
+++ Behavioral Design Pattern/Template Method/TemplateMethodCore/TemplateMethodCore/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass aA = new ConcreateClassA();
+            aA.TemplateMethod();
+
+            AbstractClass aB = new ConcreateClassB();
+            aB.TemplateMethod();
+
+            //Wait
+            Console.ReadKey();
+        }
+    }
+    /// <summary>
+    /// The 'Abstract' class
+    /// </summary>
+    abstract class AbstractClass
+    {
+        public abstract void PrimitiveOperation1();
+        public abstract void PrimitiveOperation2();
+        //The 'Template Method' 
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+            Console.WriteLine();
+        }
+    }
+    /// <summary>
+    /// The 'ConcreateClass'
+    /// </summary>
+    class ConcreateClassA : AbstractClass
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("ConcreteClassA.PrimitiveOperation1()");
+        }
+
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("ConcreteClassA.PrimitiveOperation2()");
+        }
+    }
+    /// <summary>
+    /// The 'ConcreateClass' 
+    /// </summary>
+    class ConcreateClassB : AbstractClass
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("ConcreteClassB.PrimitiveOperation1()");
+        }
+
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("ConcreteClassB.PrimitiveOperation2()");
+        }
+    }
+}
---
+++ Behavioral Design Pattern/Template Method/TemplateMethodRealWorld/TemplateMethodRealWorld/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodRealWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccessObject daoCategories = new Categories();
+            daoCategories.Run();
+
+            DataAccessObject daoProducts = new Products();
+            daoProducts.Run();
+
+            Console.ReadKey();
+        }
+    }
+    /// <summary>
+    /// The 'abstratClass' class
+    /// </summary>
+    abstract class DataAccessObject
+    {
+        protected string connectionString;
+        protected DataSet dataSet;
+
+        public virtual void Connect()
+        {
+            // Make sure mdb is available to app
+            connectionString = "provider=Microsoft.JET.OLEDB.4.0; " + "data source=..\\..\\..\\db1.mdb";
+        }
+
+        public abstract void Select();
+        public abstract void Process();
+
+        public virtual void Disconnect()
+        {
+            connectionString = "";
+        }
+        //The 'Template Method' 
+        public void Run()
+        {
+            Connect();
+            Select();
+            Process();
+            Disconnect();
+        }
+    }
+    /// <summary>
+    /// A 'ConcreateClass'
+    /// </summary>
+    class Categories : DataAccessObject
+    {
+        public override void Select()
+        {
+            string sql = "select CategoryName from Categories";
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, connectionString);
+            dataSet = new DataSet();
+            dataAdapter.Fill(dataSet, "Categories");
+        }
+
+        public override void Process()
+        {
+            Console.WriteLine("Categories ---- ");
+
+            DataTable dataTable = dataSet.Tables["Categories"];
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine(row["CategoryName"]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Products : DataAccessObject
+    {
+        public override void Process()
+        {
+            Console.WriteLine("Products ---- ");
+            DataTable dataTable = dataSet.Tables["Products"];
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine(row["ProductName"]);
+            }
+            Console.WriteLine();
+        }
+
+        public override void Select()
+        {
+            string sql = "Select ProductName from Products";
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, connectionString);
+            dataSet = new DataSet();
+            dataAdapter.Fill(dataSet, "Products");
+        }
+    }
+}
